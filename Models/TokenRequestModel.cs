3d33ebc2648912b45c77b094c69398b78e5f934c
albretsen <bjellanda@gmail.com>
2024-04-03 18:00:30 +0200

    Add project files.

---
+++ Models/TokenRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebVakt_API.Models
+{
+    public class TokenRequestModel
+    {
+        public string Code { get; set; } 
+        public string RedirectUri { get; set; } 
+    }
+}
---
+++ Models/TokenResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace WebVakt_API.Models
+{
+    public class TokenResponseModel
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
+
+        [JsonProperty("id_token")]
+        public string IdToken { get; set; }
+    }
+}
