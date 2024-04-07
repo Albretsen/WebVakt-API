a5acd1d80cad383aab38ba7800417950c95ac70c
albretsen <bjellanda@gmail.com>
2024-04-07 14:38:42 +0200

    Added UserProfile endpoint

---
+++ Controllers/UserProfileController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace WebVakt_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public ActionResult<UserProfile> Get()
+        {
+            var givenName = User.FindFirst("given_name")?.Value;
+            var familyName = User.FindFirst("family_name")?.Value;
+
+            foreach (var claim in User.Claims)
+            {
+                Console.WriteLine($"{claim.Type}: {claim.Value}");
+            }
+
+            // Assuming you have a UserProfile class that can hold this information
+            var userProfile = new UserProfile
+            {
+                GivenName = givenName,
+                FamilyName = familyName,
+            };
+
+            return Ok(userProfile);
+        }
+
+    }
+}
